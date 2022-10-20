07c75676a0ebf2dc29f6fd25c3884937e45e5e85
Acuña Axel Elias <99514456+AxelAcu-Arg@users.noreply.github.com>
2022-10-20 00:07:29 -0300

    frontend portfolio
    portfolio axel acuña

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,16 @@
+export const environment = {
+  firebase: {
+    projectId: 'frontendfinalaxel',
+    appId: '1:981089114098:web:c1cc9eaed4e27a881dfd6b',
+    storageBucket: 'frontendfinalaxel.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDLxOG0qaF2R_NDA_8RdVc6QO9asmVTlU0',
+    authDomain: 'frontendfinalaxel.firebaseapp.com',
+    messagingSenderId: '981089114098',
+    measurementId: 'G-1ES3DTZX4M',
+  },
+  production: true,
+
+  URL: 'https://backendaxel.herokuapp.com/'
+
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,28 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'frontendfinalaxel',
+    appId: '1:981089114098:web:c1cc9eaed4e27a881dfd6b',
+    storageBucket: 'frontendfinalaxel.appspot.com',
+    locationId: 'southamerica-east1',
+    apiKey: 'AIzaSyDLxOG0qaF2R_NDA_8RdVc6QO9asmVTlU0',
+    authDomain: 'frontendfinalaxel.firebaseapp.com',
+    messagingSenderId: '981089114098',
+    measurementId: 'G-1ES3DTZX4M',
+  },
+  production: false,
+
+  URL: 'https://localhost:8080'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
